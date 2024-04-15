ae8729557fc1cf1b03892f68ebf18aa935e18faf
namiranda <naylenmiranda@gmail.com>
2024-04-14 21:34:01 -0300

    feat: start migration to astro

---
+++ src/consts.ts
@@ -0,0 +1,40 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "noelia:~$",
+  EMAIL: "naylenmiranda@gmail.com",
+  NUM_POSTS_ON_HOMEPAGE: 3,
+  NUM_WORKS_ON_HOMEPAGE: 2,
+  NUM_PROJECTS_ON_HOMEPAGE: 3,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Noelia Miranda",
+  DESCRIPTION: "Welcome to my personal website. I am a software engineer and I love to build things.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const BOOKS: Metadata = {
+  TITLE: "Books",
+  DESCRIPTION: "A collection of books I have read, with reviews and ratings.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+};
+
+export const SOCIALS: Socials = [
+  { 
+    NAME: "github",
+    HREF: "https://github.com/namiranda"
+  },
+  { 
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/noelia-a-miranda/",
+  }
+];
